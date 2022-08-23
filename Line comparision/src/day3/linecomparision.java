1e1531a15dfedddc1bf2089e243e23d9c6fb4755
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-23 18:10:28 +0530

    "Welcome to Line Comparison Computation Program on Master Branch"

---
+++ Line comparision/src/day3/linecomparision.java
@@ -0,0 +1,8 @@
+package day3;
+
+public class linecomparision {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+	}
+
+}
