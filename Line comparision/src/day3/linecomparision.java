436efd1ae47e331d3fa12b92a9ee09b85cea07e3
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-23 18:26:49 +0530

    calculate length

--- Line comparision/src/day3/linecomparision.java
+++ Line comparision/src/day3/linecomparision.java
@@ -3,6 +3,14 @@ package day3;
 public class linecomparision {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+		int x1=2;
+	      int x2=4;
+	      int y1=5;
+	      int y2=10;
+	      
+	      System.out.println("A Length as 2 points : " + "("+x1+ "," +y1+ ")" + "and" + "(" +x2+ "," +y2+ ")");
+	      double LenthLine = Math.sqrt(((x2 - x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+	      System.out.println("Length of a line : "+ LenthLine);
 	}
 
 }
