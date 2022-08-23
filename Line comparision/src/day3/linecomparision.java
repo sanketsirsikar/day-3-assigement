044b3784f49b0c701a6a85b68b0bb4407ac2f28a
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-23 18:41:28 +0530

    compare two lines one line is equal, greater or less than the other line.

--- Line comparision/src/day3/linecomparision.java
+++ Line comparision/src/day3/linecomparision.java
@@ -12,24 +12,28 @@ public class linecomparision {
 		int y3 = 6;
 		int x4 = 7;
 		int y4 = 14;
-		System.out.println("A Length as 2 points : " + "("+x1+ "," +y1+ ")" + "and" + "(" +x2+ "," +y2+ ")");
-		
-        double LenthLine1 = Math.sqrt(((x2 - x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
-        
-        System.out.println("Length of a line : "+ LenthLine1);
-        
-        String str1 = Double.toString(LenthLine1);
-        
-        System.out.println("A Length as 2 points : " + "("+x3+ "," +y3+ ")" + "and" + "(" +x4+ "," +y4+ ")");
-        
-        double LenthLine2 = Math.sqrt(((x4 - x3) * (x4-x3)) + ((y4-y3) * (y4-y3)));
-        
-        System.out.println("Length of a line : "+ LenthLine2);
-        
-        String str2 = Double.toString(LenthLine2);
-        
 
-        System.out.println(str1.equals(str2));
+		double a;
+		double b;
+
+
+		System.out.println("A Length as 2 points : " + "("+x1+ "," +y1+ ")" + "and" + "(" +x2+ "," +y2+ ")");
+		double LenthLine1 = Math.sqrt(((x2 - x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+		System.out.println("Length of a line : "+ LenthLine1);
+		a = LenthLine1;
+
+		System.out.println("A Length as 2 points : " + "("+x3+ "," +y3+ ")" + "and" + "(" +x4+ "," +y4+ ")");
+		double LenthLine2 = Math.sqrt(((x4 - x3) * (x4-x3)) + ((y4-y3) * (y4-y3)));
+		System.out.println("Length of a line : "+ LenthLine2);
+		b = LenthLine2;
+
+		if (Double.compare(a, b) == 0)
+			System.out.println("LenthLine1 = LenthLine2");
+		else if (Double.compare(a, b) < 0)
+			System.out.println("LenthLine1 is lessthan LenthLine2");
+		else
+			System.out.println("LenthLine1 is graterthan LenthLine2");
+
 
 
 	}
